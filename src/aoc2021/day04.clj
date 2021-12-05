c0c2798f12849821cb4080e8865ea6bf06932713
Marc Jou <smyslov@gmail.com>
2021-12-05 14:50:56 +0100

    Day 4 part 1

---
+++ src/aoc2021/day04.clj
@@ -0,0 +1,96 @@
+(ns aoc2021.day04
+  (:require [clojure.string :as str]))
+
+(def data (slurp "resources/day04.data"))
+
+
+(def D
+"7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7")
+
+
+(defn ch->int [ch]
+  (Integer/parseInt (str ch)))
+
+
+(defn parse-board [board]
+  (->> board
+       (re-seq #"\d+")
+       (mapv (fn [v] {:number (ch->int v) :hit false}))))
+
+
+(defn parse [s]
+  (let [[raw-numbers & raw-boards] (str/split s #"\n\n")
+        numbers (->> raw-numbers
+                     (re-seq #"\d+")
+                     (mapv ch->int))
+        boards (mapv parse-board raw-boards)]
+    [numbers boards]))
+
+
+(defn board-rows [board]
+  (partition 5 board))
+
+
+(defn board-columns [board]
+  (map #(take-nth 5 (drop % board)) (range 5)))
+
+
+(defn play-board [drawn board]
+  (map (fn [row]
+         (if (= (:number row) drawn)
+           (update row :hit not)
+           row))
+       board))
+
+
+(defn winning-lines [lines]
+  (let [line-hits (map (fn [line]
+                        (every? true? (map :hit line))) lines)]
+  (some true? line-hits)))
+
+
+(defn sum-unmarked [board]
+  (apply + (map :number (remove :hit board))))
+
+
+(defn winning-board [board]
+  (let [rows (board-rows board)
+        columns (board-columns board)]
+    (if (true? (or (winning-lines rows)
+                   (winning-lines columns)))
+      board
+      nil)))
+
+(defn p1 [input]
+  (let [parsed (parse input)
+        numbers (first parsed)]
+    (loop [index 0
+           boards (map #(play-board (nth numbers index) %) (second parsed))]
+      (let [winners (filter some? (map #(winning-board %) boards))]
+        (if (seq winners)
+          (let [winner-board (first winners)
+                lucky-number (nth numbers index)
+                unmarked (sum-unmarked winner-board)]
+            (println "We have a winner")
+            (* lucky-number unmarked))
+
+          (recur (inc index)
+                 (map #(play-board (nth numbers (inc index)) %) boards)))))))
