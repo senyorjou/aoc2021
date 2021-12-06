80a0ce86840c608308803fc71c18ae976c0c16c2
Marc Jou <smyslov@gmail.com>
2021-12-06 08:56:27 +0100

    Day 5 part 2

--- src/aoc2021/day05.clj
+++ src/aoc2021/day05.clj
@@ -17,6 +17,7 @@
 0,0 -> 8,8
 5,5 -> 8,2")
 
+
 (defn ch->int [ch]
   (Integer/parseInt (str ch)))
 
@@ -35,21 +36,50 @@
   (or (= x1 x2)
       (= y1 y2)))
 
-
-(defn line-coords [[x1 y1 x2 y2]]
+(defn max-min [[x1 y1 x2 y2]]
   (let [max-x (max x1 x2)
         min-x (min x1 x2)
         max-y (max y1 y2)
         min-y (min y1 y2)]
-    (for [x1->x2 (range min-x (inc max-x))
-          y1->y2 (range min-y (inc max-y))]
-       [x1->x2 y1->y2])))
+  [min-x min-y max-x max-y]))
+
+
+(defn line-coords [line]
+  (let [[x1 y1 x2 y2] (max-min line)]
+    (for [xss (range x1 (inc x2))
+          yss (range y1 (inc y2))]
+       [xss yss])))
 
 
 (defn p1 [input]
   (->> input
        (filter hor-or-vert?)
        (mapcat line-coords)
+       (frequencies)
+       (vals)
+       (filter #(> % 1))
+       count))
+
+
+(defn diagonal-range [x1 x2]
+  (if (<= x1 x2)
+    (range x1 (inc x2))
+    (range x1 (dec x2) -1)))
+
+
+(defn diagonal-coords [[x1 y1 x2 y2]]
+  (map vector (diagonal-range x1 x2) (diagonal-range y1 y2)))
+
+
+(defn line-dispatcher [line]
+  (if (hor-or-vert? line)
+    (line-coords line)
+    (diagonal-coords line)))
+
+
+(defn p2 [input]
+  (->> input
+       (mapcat line-dispatcher)
        (frequencies)
        (vals)
        (filter #(> % 1))
