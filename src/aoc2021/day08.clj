4f5b3f376d748f6cbcd4f2f18d79b7d6ac532dcf
Marc Jou <smyslov@gmail.com>
2021-12-08 11:59:38 +0100

    Day 8 part 2 partial

--- src/aoc2021/day08.clj
+++ src/aoc2021/day08.clj
@@ -31,16 +31,79 @@ gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
         sizes (map count input)]
     (count (filter #{2 3 4 7} sizes))))
 
+;; --- Part 1
 
 (defn p1 [input]
   (apply + (map count-output (parse-data input))))
 
+;; --- Part 2
+
+(defn find-1 [input]
+  (= (count input) 2))
+
+(defn find-4 [input]
+  (= (count input) 4))
+
+(defn find-7 [input]
+  (= (count input) 3))
+
+(defn find-8 [input]
+  (= (count input) 7))
+
+(defn find-6 [input match]
+  ;; 6 is len 6 and NOT a subset of 1
+  ;; (= (count input) 6))
+  (and (= (count input) 6)
+       (not (clojure.set/subset? match (set input)))))
+
+(defn find-9 [input match]
+  ;; 9 is len 6 and a subset of 4
+  ;; (= (count input) 6))
+  (and (= (count input) 6)
+       (clojure.set/subset? match (set input))))
+
+
+(defn get-set [n line]
+  (let [matches (filter #(= n (count %)) line)]
+    (first (map set matches))))
+
+(defn get-sub [n line]
+  (filter #(= n (count %)) line))
+
+
+(defn get-nums [line]
+  (let [set-1 (get-set 2 line)
+        set-4 (get-set 4 line)
+        set-7 (get-set 3 line)
+        set-8 (get-set 7 line)
+        sub-069 (get-sub 6 line)
+        leds-6 (filter #(find-6 % set-1) sub-069)
+        leds-9 (filter #(find-9 % set-4) sub-069)]
+
+    [set-1 set-4 set-7 set-8 leds-6 leds-9 sub-069]))
 
 (comment
 
   (apply + (map count-output (parse-data D)))
-  (->> (parse-data D)
-       (map second)
-       (filter )
-       (map count))
-)
+
+  ;;
+  (def bits-groups {
+                    2 {1 "0010010"}
+                    3 {7 "1010010"}
+                    4 {4 "0111010"}
+                    5 {2 "1011101" 3 "1011011" 5 "1101011"}
+                    6 {0 "1110111" 6 "1101111" 9 "1111011"}
+                    7 {8 "1111111"}})
+
+  (def bits {
+        0 "1110111"
+        1 "0010010"
+        2 "1011101"
+        3 "1011011"
+        4 "0111010"
+        5 "1101011"
+        6 "1101111"
+        7 "1010010"
+        8 "1111111"
+        9 "1111011"})
+  )
