f99a2324f14b28d975c1b7e6b94afb9bc456e686
Marc Jou <smyslov@gmail.com>
2021-12-10 10:24:28 +0100

    Day 10 part 2

--- src/aoc2021/day10.clj
+++ src/aoc2021/day10.clj
@@ -27,11 +27,18 @@
             \( \)
             \< \>})
 
-(def points {
-             \) 3
-             \] 57
-             \} 1197
-             \> 25137})
+(def points-p1 {
+                \) 3
+                \] 57
+                \} 1197
+                \> 25137})
+
+(def points-p2 {
+                \) 1
+                \] 2
+                \} 3
+                \> 4})
+
 
 (defn process-line
   ([sequence]
@@ -39,7 +46,7 @@
   ([sequence stack]
     (let [token (first sequence)]
       (if (nil? token) ;; last
-        nil
+        [nil stack]
         (if (pairs token) ;; is opening, push and continue
           (recur (rest sequence)
                  (conj stack token))
@@ -47,22 +54,29 @@
             (recur (rest sequence)
                    (pop stack))
             ;; none of the above... this is the guy
-            token))))))
+            [token nil]))))))
 
+
+(defn rating [acc v]
+  (+ (* acc 5) v))
+
+(defn get-p2-points [tokens]
+  (let [closing (reverse (map pairs tokens))]
+    (reduce rating (map points-p2 closing))))
 
 (defn p1 [input]
   (->> input
        (map process-line)
+       (map first)
        (filter some?)
-       (map points)
+       (map points-p1)
        (reduce +)))
 
-
-
-(comment
-  (def line [\[ \( \{ \( \< \( \( \) \) \[ \] \> \[ \[ \{ \[ \] \{ \< \( \) \< \> \]])
-  (get pairs (first line))
-
-  (balance line)
-  (def brackets {\[ \] \( \) \{ \} \< \>})
-  )
+(defn p2 [input]
+  (let [values (->> input
+                    (map process-line)
+                    (map second)
+                    (filter some?)
+                    (map get-p2-points))
+        middle (quot (count values) 2)]
+    (first (drop middle (sort values)))))
